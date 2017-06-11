a74e52856721d7947b16a7f6737c4a015fe27db3
hyginn <boris.steipe@utoronto.ca>
2017-06-11 19:51:09 -0400

    myScript.R template

---
+++ tmp.R
@@ -0,0 +1,22 @@
+# myScript.R
+#
+# Write your notes and code experiments into this document. Save it
+# from time to time - however I recommend that you do not _commit_ 
+# your saved version.
+#
+# As long as you do not _commit_ this script to version control, 
+# you can _pull_ updated versions of the entire project from GitHub
+# by using the RStudio version control interface. However, once 
+# you _commit_ any file in your local version, RStudio will require
+# you to resolve conflicts before you can _pull_ updates.
+#
+# ====================================================================
+
+
+
+
+
+
+
+# [END]
+
