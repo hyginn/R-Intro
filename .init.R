157b7f48f345cc7c4523109d085149adac35a3ed
hyginn <boris.steipe@utoronto.ca>
2017-06-11 19:50:26 -0400

    Add logic for myScript.R

--- .init.R
+++ .init.R
@@ -3,6 +3,13 @@
 # Boris Steipe
 # ====================================================================
 
+# Create a local copy of myScript.R if that hasn't been done yet.
+if (! file.exists("myScript.R")) {
+    file.copy("tmp.R", "myScript.R")
+}
+
+# source(".utilities.R")
+
 file.edit("R-Intro.R")
 
 # [End]
