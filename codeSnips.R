fcd2afece10c701d6f887d0b97a58a7ffe16af18
hyginn <boris.steipe@utoronto.ca>
2018-05-14 23:35:16 -0400

    Update code snips

--- codeSnips.R
+++ codeSnips.R
@@ -1,5 +1,7 @@
 # codeSnips.R
 
+# ===== A function to make dinucleotides =======================================
+
 dinucVector <- function(x) {
     # create a vector of dinucleotides from a sequence vector
     # (pedestrian version)
@@ -20,7 +22,49 @@ tObs <- table(dinuc)
 dinucRand <- dinucVector(sample(mySeq))
 tRand <- table(dinucRand)
 
-# Barplot things:
+# ====== Impoving the function =================================================
+# Better code:
+# The less "pedestrian version" is to use paste in asmarter way.
+# Vectors that are paste()'ed are joined element by element:
+
+paste(c("A", "B", "C"), c("D", "E", "F"), sep = ":")  # "A:D" "B:E" "C:F"
+paste(1:3, 4:6, 7:9)   # "1 4 7" "2 5 8" "3 6 9"
+
+# Exmple: paste together all 64 codons from different repeating patterns of
+# nucleotides
+
+# define
+n1 <-     c(rep("A",16), rep("C",16), rep("G",16), rep("T",16)))
+n2 <- rep(c(rep("A", 4), rep("C", 4), rep("G", 4), rep("T", 4)), 4))
+n3 <- rep(c(    "A",         "C",         "G",         "T"),    16))
+
+# confirm
+paste(n1, collapse = "")
+paste(n2, collapse = "")
+paste(n3, collapse = "")
+
+# collate
+paste(n1, n2, n3,  sep = "")
+
+
+
+# So we can write a super simple expression for our dinucleotides
+
+dinucVector <- function(x) {
+    # create a vector of dinucleotides from a nucleotide sequence vector
+    idx <- 1:(length(x) - 1)
+    return(paste(x[idx], x[idx + 1], sep=""))
+}
+
+# test it:
+dinucVector(mySeq[1:20])
+
+# Task: - can you make a function for trinucleotides?
+#       - can you make non-overlapping trinucleotides (i.e. collate a
+#         sequence vector into codons?)
+
+
+# ====== Barplot things ========================================================
 sortedNames <- names(sort(tObs, decreasing = TRUE))
 
 tObs[sortedNames]
