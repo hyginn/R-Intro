33585b1f9a3300126f011a87beec599b131a8046
hyginn <boris.steipe@utoronto.ca>
2018-05-15 15:19:41 -0400

    share more model code

--- codeSnips.R
+++ codeSnips.R
@@ -192,6 +192,158 @@ table(GNAS2protein$iCodon[sel]) # Yes. That would be difficult to tell from
 # the mutation table, because different
 # transcripts have different lengths.
 
+# =    5  PLOT THE DATA  =======================================================
+#
+# Time for a Lolliplot
+
+# Task 5.1 What categories of effects do we have?
+unique(GNAS2mut$MOST_SEVERE)
+
+# Task 5.2 Define colors for the categories - (Hint: pick a palette e.g. with
+#          https://color.adobe.com/ You are looking for a divergent spectrum
+#          that emphasizes similar vs. different effects.
+#
+#          e.g. "#D42823AA"      # "frameshift_variant"
+#               "#FC7B14AA"      # "missense_variant"
+#               "#ED69A7AA"      # "stop_gained"
+#               "#CAD1FAAA"      # "synonymous_variant"
+#
+#          - also define a color for a rectangle that symbolizes the
+#            protein:
+colProtein <- "#82A2D1"
+
+#          - to work with the effect categories, put them into a
+#            data frame: eff$effects - the effects
+#                        eff$cols    - the colours
+#                        eff$heights - the vertical positions
+#          - give the data frame rownames of the effects, so it's easy
+#            to fetch data by rowname
+
+eff <- data.frame( effects = sort(unique(GNAS2mut$MOST_SEVERE)),
+                   cols = c("#D42823A3",   # "frameshift_variant"
+                            "#FC7B14A3",   # "missense_variant"
+                            "#ED69A7A3",   # "stop_gained"
+                            "#CAD1FAA3"),  # "synonymous_variant"
+                   heights = c(1.7, 3.0, 2.3, 1.0),
+                   stringsAsFactors = FALSE)
+rownames(eff) <- eff$effect
+
+#  >>> let's verify our colors: looking good?
+barplot(rep(10, 4),
+        axes = FALSE,
+        names.arg = eff$effects,
+        las = 2,
+        cex.names = 0.5,
+        col = eff$cols)
+
+
+# Task 5.3 Compile the mutations by amino acid and mutation type.
+#          - define a matrix with rows for each mutated position,
+#            columns for each effect category. Give it rownames() of
+#            positions, colnames() of effects - so we can easily
+#            access data by position and mutation type. Call the matrix
+#            mMut
+
+sel <- which(GNAS2protein$coords %in% GNAS2mut$START)
+positions <- unique(GNAS2protein$iCodon[sel])
+mMut <- matrix(numeric(length(positions) * nrow(eff)), ncol = nrow(eff))
+rownames(mMut) <- positions
+colnames(mMut) <- eff$effects
+
+#          - iterate over all mutations, find which sequence position it
+#            affects with which effect, and increment the value you find
+#            in the mMut matrix.
+
+for (i in 1:nrow(GNAS2mut)) {
+    sel <- which( GNAS2protein$coords  %in% GNAS2mut$START[i])
+    pos <- as.character(GNAS2protein$iCodon[sel])
+    e <- GNAS2mut$MOST_SEVERE[i]
+    mMut[pos, e] <- mMut[pos, e] + 1
+}
+
+# Task 5.4 Prepare for plotting.
+#          - How do we draw circles on the plot?
+
+# >>> We could use the draw.circle() function in the
+# plotrix package - but that won't work if we don't take care to set the aspect
+# ratio of the plot to 1. Better to use plotting symbols and vary their size.
+# The symbol we need is number 21:
+plot(1:4,
+     rep(1,4),
+     pch = 21,
+     xlim = c(0, 5),
+     axes = FALSE, xlab = "", ylab = "",
+     cex = seq(1, 8, length.out = 4),
+     col = "#555555",
+     bg = eff$cols)
+
+
+#          - What size should the circles have?
+# >>> write a function to calculate plot symbol sizes
+n2cex <- function(n, nMax) {
+    cMax <- 5
+    return((n / nMax) * cMax)
+}
+
+#          - How do we put graphic elements on a plot in principle?
+#          (Hint: draw an empty plot of the correct size, then add
+#                 lines(), points(), rectangle(), polygon() or text().
+#                 Also add axes(). And a legend. And a title.)
+
+
+
+# Task 5.4 Define a layout - x, and y ddimensions
+# >>> x- axis from 0 to sequence length + 1
+#     y axis from 0 to 5, centering the four circle categories on 1 to 4
+#
+# Task 5.5. Plot ...
+#           - an empty frame to setup the coordinates...
+plot(0, 0,
+     type = "n",
+     xlim = c(0, ceiling(max(GNAS2protein$iCodon) / 100) * 100),
+     ylim = c(0, nrow(eff) + 2),
+     xlab = "GNAS-2 sequence position",
+     ylab = "",
+     main = "CDS mutations by effect and frequency",
+     axes = FALSE)
+
+#            - draw a rectangle for the protein ...
+rect(xleft = 1,
+     ybottom = 0,
+     xright = max(GNAS2protein$iCodon),
+     ytop = 0.15,
+     col = colProtein)
+
+#           - and an axis at the bottom ...
+axis(side = 1)
+
+
+#            -  then plot the mutations for all positions and categories ...
+nMax <- max(mMut)
+
+for (i in 1:nrow(mMut)) {
+    for (j in 1:ncol(mMut)) {
+        if ((n <- mMut[i, j]) > 0) {
+            x <- as.numeric(rownames(mMut)[i])
+            y <- eff[colnames(mMut)[j], "heights"]
+            thisCol <- eff[colnames(mMut)[j], "cols"]
+            # draw a line
+            lines(c(x, x), c(0.15, y), col = "#00000033")
+            # draw the circle
+            points(x, y, pch = 21, cex = n2cex(n, nMax), bg = thisCol)
+        }
+    }
+}
+
+#            - finally, plot a legend
+# >>> order it correctly
+ord <- order(eff$heights, decreasing = TRUE)
+legend("topright",
+       eff$effects[ord],
+       cex = 0.9,
+       fill = eff$cols[ord],
+       bty = 'n',
+       border = "#000000")
 
 
 
